c54075948139ea37fc697cccabd3760d977baff5
Alvaro <alvarop281@gmail.com>
2021-03-01 18:55:30 -0400

    upload project to github

--- README.md
+++ README.md
@@ -25,3 +25,6 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+# Pokedex
+
+API consumption practice using Sass and Angular (TypeScript, Flex Layout, ngx-infinite-scroll, ngx-spinner and ngrx)
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,10 @@ import { Error500Component } from './pages/errors/error500/error500.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', component: DashboardComponent },
   { path: '404', component: Error404Component },
   { path: '500', component: Error500Component },
-  { path: '', component: DashboardComponent }
+  { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
