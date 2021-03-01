0e80cf0210a2bfb562a5cd72fb6d88280c9da963
Alvaro <alvarop281@gmail.com>
2021-03-01 14:21:59 -0400

    Debug appStore

--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -1,9 +1,7 @@
 import { PokeInfo } from './models/PokeInfo';
-import { Pokemon } from './models/Pokemon';
-
 
 export interface AppState{
     readonly device: boolean;
     readonly pokeList: PokeInfo[];
-    readonly pokeSelected: Pokemon;
+    readonly pokeSelected: string;
 }
